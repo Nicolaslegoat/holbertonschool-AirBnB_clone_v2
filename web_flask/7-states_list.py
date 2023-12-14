29abe5645b72aae51aed3af70d5179340ffdce7a
Nicolaslegoat <n.mancino03@gmail.com>
2023-12-14 14:32:47 -0800

    pycode

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -14,6 +14,7 @@ def teardown(exception):
     """Remove the current session"""
     storage.close()
 
+
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """Display a HTML page with a list of all State objects in DBStorage"""
@@ -21,6 +22,7 @@ def states_list():
     sorted_states = sorted(states, key=lambda state: state.name)
 
     return render_template("7-states_list.html", states=sorted_states)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
